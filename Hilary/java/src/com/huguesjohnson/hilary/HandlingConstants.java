21628582bf4a7f6844eaa5f4fa34eef620e1fc66
Hugues Johnson <hugues@huguesjohnson.com>
2026-02-28 09:03:26 -0600

    Create HandlingConstants.java
    VN6 -> Java conversion

---
+++ Hilary/java/src/com/huguesjohnson/hilary/HandlingConstants.java
@@ -0,0 +1,44 @@
+/* https://github.com/huguesjohnson/powerups/blob/main/LICENSE */
+
+package com.huguesjohnson.hilary;
+
+/*
+ * Originally written in Visual Basic 6, converted to Java by Google Gemini.
+ */
+
+public abstract class HandlingConstants{
+	//digit 1 
+    public static final int G1_BOOST=1;
+    public static final int G2_BOOST=2;
+    public static final int REV_BONNET=4;
+    public static final int HANGING_BOOT=8;
+    //digit 2
+    public static final int NO_DOORS=1;
+    public static final int IS_VAN=2;
+    public static final int IS_BUS=4;
+    public static final int IS_LOW=8;
+    //digit 3
+    public static final int DBL_EXHAUST=1;
+    public static final int TAILGATE_BOOT=2;
+    public static final int NOSWING_BOOT=4;
+    public static final int NONPLAYER_STABILISER=8;
+    //digit 4
+    public static final int NEUTRALHANDLING=1;
+    public static final int HAS_NO_ROOF=2;
+    public static final int IS_BIG=4;
+    public static final int HALOGEN_LIGHTS=8;
+    //digit 5
+    public static final int IS_BIKE=1;
+    public static final int IS_HELI=2;
+    public static final int IS_PLANE=4;
+    public static final int IS_BOAT=8;
+    //digit 6
+    public static final int NO_EXHAUST=1;
+    public static final int REARWHEEL_1ST=2;
+    public static final int HANDBRAKE_TYRE=4;
+    public static final int SIT_IN_BOAT=8;
+    //digit 7
+    public static final int FAT_REARW=1;
+    public static final int NARROW_FRONTW=2;
+    public static final int GOOD_INSAND=4;
+}
